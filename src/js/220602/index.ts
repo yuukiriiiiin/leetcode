9fb3d9840158e8e73c2760a6c6b4e20127ae76e7
yuki sakata <sakata_yuuki@waku-2.com>
2022-06-02 02:07:56 +0900

    fix: roman-to-integer

--- src/js/220602/index.ts
+++ src/js/220602/index.ts
@@ -1,8 +1,34 @@
 export const romanToInt = (s: string): number => {
-  const strArr: string[] = [...s];
-  let total: number = 0;
+  type RomanType =
+    | "I"
+    | "V"
+    | "X"
+    | "L"
+    | "C"
+    | "D"
+    | "M"
+    | "IV"
+    | "IX"
+    | "XL"
+    | "XC"
+    | "CD"
+    | "CM";
+  const RomanIregularType = ["IV", "IX", "XL", "XC", "CD", "CM"] as RomanType[];
+  const romanArr: RomanType[] = [];
 
-  const obj = {
+  const str = RomanIregularType.reduce((prev, cur) => {
+    if (s.includes(cur)) {
+      romanArr.push(cur);
+      return prev.replace(cur, "");
+    }
+    return prev;
+  }, s);
+
+  const newRomanArr = [...romanArr, ...str] as RomanType[];
+
+  const conversionTable: {
+    [key in RomanType]: number;
+  } = {
     I: 1,
     V: 5,
     X: 10,
@@ -10,29 +36,17 @@ export const romanToInt = (s: string): number => {
     C: 100,
     D: 500,
     M: 1000,
+    IV: 4,
+    IX: 9,
+    XL: 40,
+    XC: 90,
+    CD: 400,
+    CM: 900,
   };
-  for (const s of strArr) {
-    if (s === "I") {
-      total += 1;
-    }
-    if (s === "V") {
-      total += 5;
-    }
-    if (s === "X") {
-      total += 10;
-    }
-    if (s === "L") {
-      total += 50;
-    }
-    if (s === "C") {
-      total += 100;
-    }
-    if (s === "D") {
-      total += 500;
-    }
-    if (s === "M") {
-      total += 1000;
-    }
-  }
+
+  const total = newRomanArr.reduce((prev: number, cur, i) => {
+    return prev + conversionTable[cur];
+  }, 0);
+
   return total;
 };
