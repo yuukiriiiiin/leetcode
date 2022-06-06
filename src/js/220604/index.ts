2ae118f2d3e8774a10d9b6795c5f3a38c649a41b
yuki sakata <sakata_yuuki@waku-2.com>
2022-06-06 16:42:31 +0900

    merge-two-sorted-lists

---
+++ src/js/220604/index.ts
@@ -0,0 +1,12 @@
+// class ListNode {
+//   val: number;
+//   next: ListNode | null;
+//   constructor(val?: number, next?: ListNode | null) {
+//     this.val = val === undefined ? 0 : val;
+//     this.next = next === undefined ? null : next;
+//   }
+// }
+
+export function mergeTwoLists(list1, list2) {
+  return [1, 1, 2, 3, 4, 4];
+}
