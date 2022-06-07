da2fbcbd927f09e98d9ccabf30565ee768c4df62
yuki sakata <sakata_yuuki@waku-2.com>
2022-06-07 10:51:32 +0900

    fix: valid-parentheses

--- src/js/220607/index.ts
+++ src/js/220607/index.ts
@@ -4,6 +4,22 @@ export const isValid = (s: string) => {
   const arr = s.match(/.{2}/g) as string[];
 
   const result = [];
+
+  // 文字列を半分に分割する
+  const prev = s.slice(0, s.length / 2);
+  const cur = s.slice(s.length / 2, s.length);
+
+  // 2番目のグループを、それぞれ対応するカッコに戻す
+  const reversed = [...cur].reduceRight((p, c) => {
+    console.log(p, c);
+    return p;
+  });
+
+  // 1番目と2番目のグループが一致していなかったら、
+
+  // もし一致してなかったら、グループを2つずつに分割して、比べる作業
+  // 繰り返す
+
   for (const item of arr) {
     if (item[0] === "(") {
       result.push(item[1] === ")");
