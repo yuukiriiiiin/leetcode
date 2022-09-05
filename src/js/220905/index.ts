47e29122b4321a6982c77ef7f47ad94ae64f2898
yuki sakata <sakata_yuuki@waku-2.com>
2022-09-05 13:54:12 +0900

    test2

--- src/js/220905/index.ts
+++ src/js/220905/index.ts
@@ -1,6 +1,7 @@
 export const searchInsert = (nums:number[], target:number): number => {
   if(nums.includes(target)) {
-    return nums.indexOf(target)
+    return nums.indexOf(target);
+  } else {
+    return [...nums, target].sort((a , b) => (a - b)).indexOf(target);
   }
-  return 2
 };
