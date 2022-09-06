f663fd02c3ca0953758d5ef640c292ba33ee9a43
yuki sakata <sakata_yuuki@waku-2.com>
2022-09-06 12:49:53 +0900

    test 修正（途中）

--- src/js/220607/index.ts
+++ src/js/220607/index.ts
@@ -1,47 +1,16 @@
-export const isValid = (s: string) => {
+export const isValid = (s: string): boolean => {
   // 文字数が奇数だったら false
   if (s.length % 2 !== 0) return false;
-  const arr = s.match(/.{2}/g) as string[];
 
-  const result = [];
+  const reverse = s.split("").reverse().join("");
+  if (s === reverse) return true;
 
-  // 文字列を半分に分割する
-  const prev = s.slice(0, s.length / 2);
-  const cur = s.slice(s.length / 2, s.length);
 
-  // 2番目のグループを、それぞれ対応するカッコに戻す
-  const reversed = [...cur].reduceRight((p, c) => {
-    if (c === ")") {
-      c = "(";
-    } else if (c === "]") {
-      c = "[";
-    } else if (c === "}") {
-      c = "{";
-    } else {
-      c = "not equal";
-    }
-    return p + c;
-  }, "");
+  const arr = s.match(/.{2}/g);
 
-  // 1番目と2番目のグループが一致していたら、
-  if (prev === reversed) {
-    return true;
-  }
+  if (arr === null) return false;
 
-  // もし一致してなかったら、グループを2つずつに分割して、比べる作業
-  // 繰り返す
 
-  for (const item of arr) {
-    if (item[0] === "(") {
-      result.push(item[1] === ")");
-    }
-    if (item[0] === "[") {
-      result.push(item[1] === "]");
-    }
-    if (item[0] === "{") {
-      result.push(item[1] === "}");
-    }
-  }
 
-  return result.every((r) => r === true);
+  return arr.every(item => (item.charAt(0) === '(' && item.charAt(1) === ')') || (item.charAt(0) === '{' && item.charAt(1) === '}') || (item.charAt(0) === '[' && item.charAt(1) === ']'));
 };
