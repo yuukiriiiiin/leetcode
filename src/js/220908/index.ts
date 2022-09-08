8a91ed27ed95e97d5496ba53a54c41714db83cc3
yuki sakata <sakata_yuuki@waku-2.com>
2022-09-08 13:42:55 +0900

    climbStairs

---
+++ src/js/220908/index.ts
@@ -0,0 +1,7 @@
+const cache: number[] = []
+
+export const climbStairs = (n: number): number => {
+  if (n <= 2) return n
+  if (!cache[n]) { cache[n] = climbStairs(n - 2) + climbStairs(n - 1) }
+  return cache[n]
+};
