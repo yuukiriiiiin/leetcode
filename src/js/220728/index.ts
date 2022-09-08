d4a6984da6935dea3210b8d088d64bb967b11662
yuki sakata <sakata_yuuki@waku-2.com>
2022-09-08 12:40:21 +0900

    コード修正

--- src/js/220728/index.ts
+++ src/js/220728/index.ts
@@ -1,4 +1,8 @@
 export const removeElement = (nums: number[], val: number): number => {
-  return nums.splice(0, nums.length, ...nums.filter((n: number) => n !== val))
-    .length;
+  // return nums.splice(0, nums.length, ...nums.filter((n: number) => n !== val)).length;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) { nums.splice(i, 1); i-- }
+  }
+
+  return nums.length
 };
