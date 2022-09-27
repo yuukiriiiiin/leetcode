eb4591627fcca6edb849000c6ab9bdff589b6769
yuki sakata <sakata_yuuki@waku-2.com>
2022-09-27 18:48:26 +0900

    isValid を修正

--- src/js/220607/index.ts
+++ src/js/220607/index.ts
@@ -1,47 +1,24 @@
-export const isValid = (s: string) => {
-  // 文字数が奇数だったら false
-  if (s.length % 2 !== 0) return false;
-  const arr = s.match(/.{2}/g) as string[];
+export const isValid = (s: string): boolean => {
+  const hash: { [key: string]: string } = {
+    '(': ')',
+    '{': '}',
+    '[': ']',
+  }
 
-  const result = [];
+  let stack: string[] = [];
 
-  // 文字列を半分に分割する
-  const prev = s.slice(0, s.length / 2);
-  const cur = s.slice(s.length / 2, s.length);
-
-  // 2番目のグループを、それぞれ対応するカッコに戻す
-  const reversed = [...cur].reduceRight((p, c) => {
-    if (c === ")") {
-      c = "(";
-    } else if (c === "]") {
-      c = "[";
-    } else if (c === "}") {
-      c = "{";
+  for (const char of s) {
+    if (char in hash) {
+      stack.push(char);
     } else {
-      c = "not equal";
-    }
-    return p + c;
-  }, "");
-
-  // 1番目と2番目のグループが一致していたら、
-  if (prev === reversed) {
-    return true;
-  }
-
-  // もし一致してなかったら、グループを2つずつに分割して、比べる作業
-  // 繰り返す
-
-  for (const item of arr) {
-    if (item[0] === "(") {
-      result.push(item[1] === ")");
-    }
-    if (item[0] === "[") {
-      result.push(item[1] === "]");
-    }
-    if (item[0] === "{") {
-      result.push(item[1] === "}");
+      const top = stack.pop();
+      console.log(top)
+      if (top === undefined || hash[top] !== char) {
+        return false;
+      }
     }
   }
 
-  return result.every((r) => r === true);
+  return !stack.length;
+
 };
